fdbc4f76b5734e7619702d9b6531831771d99bc7
chasejh <chasejh@plu.edu>
2017-11-29 18:11:21 -0800

    Model modifications

--- GradSchooler/Models/Program.cs
+++ GradSchooler/Models/Program.cs
@@ -3,6 +3,9 @@ namespace GradSchooler.Models
 {
     public class Program
     {
+        public string Name { get; set; }
+        public char Degree { get; set; }
+
         public Program()
         {
         }
