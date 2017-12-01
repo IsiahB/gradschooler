465b2092de69d852d3a37fe8a409180d6803ced8
behneril <behneril@plu.edu>
2017-11-30 18:42:34 -0800

    Attempted to add the ability to create an account through the website.

--- GradSchooler/Models/Account.cs
+++ GradSchooler/Models/Account.cs
@@ -13,28 +13,7 @@ namespace GradSchooler.Models
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string accType { get; set; }
-        public string birthday { get; set; }
-        
-        public Account(string emailA, string passwordA, string firstNameA,
-            string lastNameA, string accTypeA, string birthdayA)
-        {
-            email = emailA;
-            password = passwordA;
-            firstName = firstNameA;
-            lastName = lastNameA;
-            accType = accTypeA;
-            birthday = birthdayA;
-        }
-
-        public void setEmail()
-        {
-            
-        }
-
-       
-
-        
-
+        public string birthday { get; set; }   
 
     }
 }
