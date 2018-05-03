8f48751109aaeae8255dd8d71d8c9af4d4a4bb33
behneril <behneril@plu.edu>
2018-05-02 19:47:20 -0700

    Program view page now displays 15 programs per page.

--- GradSchooler/Controllers/ProgramController.cs
+++ GradSchooler/Controllers/ProgramController.cs
@@ -12,15 +12,23 @@ namespace GradSchooler.Controllers
         private List<Program> progs = null;
 
         // GET: Program
-        public ActionResult Program()
+        public ActionResult Program(int page = 0)
         {
+            const int pageSize = 15; //number of elements per page
+
             ViewBag.Message = "Program Page";
 
             DBUtilities.DBUtilities db = DBUtilities.DBUtilities.Instance;
             progs = new List<Program>();
+
             progs = db.getPrograms("", "");
-            ViewData["programs"] = progs;
-            return View();
+            var count = progs.Count;
+            var data = progs.Skip(page * pageSize).Take(pageSize).ToList();
+            ViewBag.MaxPage = (count / pageSize) - (count % pageSize == 0 ? 1 : 0);
+            ViewBag.Page = page;
+
+            ViewData["programs"] = data;
+            return View(data);
         }
         /// <summary>
         /// Called when a user is searching for a specific program
