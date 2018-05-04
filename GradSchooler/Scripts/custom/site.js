79ee10ef6d71d377c2237fbe9bd78e674c74afb9
Jessica Chase <chasejh@plu.edu>
2018-05-03 19:21:17 -0700

    Fixed the University nested forms, about, profile

--- GradSchooler/Scripts/custom/site.js
+++ GradSchooler/Scripts/custom/site.js
@@ -1,12 +1,14 @@
 ﻿$(document).ready(function() {
   $('.submit_on_enter').keydown(function(event) {
-    // enter has keyCode = 13, change it if you want to use another button
     if (event.keyCode == 13) {
-      this.form.submit();
+        this.form.submit();
+      }
       return false;
     }
   });
 });
+
+
 
 $('.delete').on("click", function (e) {
     e.preventDefault();
